e51ffeaa379d8c2572ad22ae96a502bf63c41b16
Ruben Das <ruben.das@tink.com>
2021-11-19 16:01:24 +0000

    Actually implemented the logic required for FizzBuzz and made all the tests pass.
    --- sturdy ---
    user_id: cce58b20-172b-4b4f-b799-209e2166c630
    view_id: b20aeea4-0122-4a55-a71f-db2b20866dff
    workspace_id: 01142e6f-b462-4c9a-a7ab-f213938e3df5

--- pkg/fizzbuzz/fizzbuzz.go
+++ pkg/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,23 @@
 package fizzbuzz
 
+import "strconv"
+
 func UpToLength(length int) []string {
 	out := []string{}
+	for i := 1; i <= length; i++ {
+		out = append(out, digitToFizzBuzz(i))
+	}
 	return out
+}
+
+func digitToFizzBuzz(input int) string {
+	if input%3 == 0 && input%5 == 0 {
+		return "FizzBuzz"
+	} else if input%3 == 0 {
+		return "Fizz"
+	} else if input%5 == 0 {
+		return "Buzz"
+	} else {
+		return strconv.Itoa(input)
+	}
 }
