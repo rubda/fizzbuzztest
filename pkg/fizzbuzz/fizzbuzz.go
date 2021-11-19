4bf443f247b88094bfa909c584142c3344f80e48
Kiril Videlov <kiril@videlov.com>
2021-11-19 16:35:11 +0000

    delete stuff
    --- sturdy ---
    user_id: 2be622cc-03ce-41fc-a2fa-13daff3d1d53
    view_id: 146d3b56-1651-492e-882b-e4def0699765

--- pkg/fizzbuzz/fizzbuzz.go
+++ pkg/fizzbuzz/fizzbuzz.go
@@ -23,10 +23,4 @@ func digitToFizzBuzzOMGNICE(input int) string {
 	if input%3 == 0 && input%5 == 0 {
 		return "fizzbuzz"
 	} else if input%3 == 0 {
-		return "fizz"
-	} else if input%5 == 0 {
-		return "buzz"
-	} else {
-		return strconv.Itoa(input)
-	}
-}
+		
