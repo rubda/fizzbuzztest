8865b4c092bbd1c11dca389f15228b33d8e21839
Kiril Videlov <kiril@videlov.com>
2021-11-19 16:33:06 +0000

    ertghjhgfdfgh
    --- sturdy ---
    user_id: 2be622cc-03ce-41fc-a2fa-13daff3d1d53
    view_id: 146d3b56-1651-492e-882b-e4def0699765

--- pkg/fizzbuzz/fizzbuzz.go
+++ pkg/fizzbuzz/fizzbuzz.go
@@ -2,15 +2,24 @@ package fizzbuzz
 
 import "strconv"
 
-func UpToLength(length int) []string {
+
+
+
+
+
+
+
+
+
+func UpToLength(length int) []string {fdsfdsfds
 	out := []string{}
 	for i := 1; i <= length; i++ {
-		out = append(out, digitToFizzBuzz(i))
+		out = append(out, digitToFizzBuzzOMGNICE(i))
 	}
 	return out
 }
 
-func digitToFizzBuzz(input int) string {
+func digitToFizzBuzzOMGNICE(input int) string {
 	if input%3 == 0 && input%5 == 0 {
 		return "fizzbuzz"
 	} else if input%3 == 0 {
